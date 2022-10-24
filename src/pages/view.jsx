09f41da25e98b3d9405be88193c18666c93a5b8d
Bodhitha Weerasekara <88415912+IT20204334@users.noreply.github.com>
2022-10-24 10:34:16 +0530

    fix table

--- src/pages/view.jsx
+++ src/pages/view.jsx
@@ -1,4 +1,4 @@
-import { Table, message } from "antd";
+import { Table } from "antd";
 import axios from "axios";
 import React, { useState, useEffect } from "react";
 //import useRequest from "../../services/RequestContext";
@@ -98,6 +98,121 @@ const View = () => {
       title: "KW3",
       dataIndex: "kW3",
       key: "kW3",
+    },
+    {
+      title: "KVA1",
+      dataIndex: "kVA1",
+      key: "kVA1",
+    },
+    {
+      title: "KVA2",
+      dataIndex: "kVA2",
+      key: "kVA2",
+    },
+    {
+      title: "KVA3",
+      dataIndex: "kVA3",
+      key: "kVA3",
+    },
+    {
+      title: "KVAr1",
+      dataIndex: "kVAr1",
+      key: "kVAr1",
+    },
+    {
+      title: "KVAr2",
+      dataIndex: "kVAr2",
+      key: "kVAr2",
+    },
+    {
+      title: "KVAr3",
+      dataIndex: "kVAr3",
+      key: "kVAr3",
+    },
+    {
+      title: "TotalKW",
+      dataIndex: "totalKW",
+      key: "totalKW",
+    },
+    {
+      title: "TotalKVA",
+      dataIndex: "totalKVA",
+      key: "totalKVA",
+    },
+    {
+      title: "TotalKVAr",
+      dataIndex: "totalKVAr",
+      key: "totalKVAr",
+    },
+    {
+      title: "PF1",
+      dataIndex: "pF1",
+      key: "pF1",
+    },
+    {
+      title: "PF2",
+      dataIndex: "pF2",
+      key: "pF2",
+    },
+    {
+      title: "PF3",
+      dataIndex: "pF3",
+      key: "pF3",
+    },
+    {
+      title: "AveragePF",
+      dataIndex: "averagePF",
+      key: "averagePF",
+    },
+    {
+      title: "Frequency",
+      dataIndex: "frequency",
+      key: "frequency",
+    },
+    {
+      title: "TotalNetKVAh",
+      dataIndex: "totalNetKVAh",
+      key: "totalNetKVAh",
+    },
+    {
+      title: "TotalNetKVArh",
+      dataIndex: "totalNetKVArh",
+      key: "totalNetKVArh",
+    },
+    {
+      title: "KWMaxActivePower",
+      dataIndex: "kWMaxActivePower",
+      key: "kWMaxActivePower",
+    },
+    {
+      title: "KWMinActivePower",
+      dataIndex: "kwMinActivePower",
+      key: "kwMinActivePower",
+    },
+    {
+      title: "KVArMaxReactivePower",
+      dataIndex: "kVArMaxReactivePower",
+      key: "kVArMaxReactivePower",
+    },
+    {
+      title: "KVArMinReactivePower",
+      dataIndex: "kVArMinReactivePower",
+      key: "kVArMinReactivePower",
+    },
+    {
+      title: "KVAMaxApparentPower",
+      dataIndex: "kVAMaxApparentPower",
+      key: "kVAMaxApparentPower",
+    },
+    {
+      title: "NeturalCurrent",
+      dataIndex: "neturalCurrent",
+      key: "neturalCurrent",
+    },
+    {
+      title: "Time",
+      dataIndex: "time",
+      key: "time",
     },
   ];
 
